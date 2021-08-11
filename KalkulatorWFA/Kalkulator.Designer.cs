c51b587e520d24d8c6f27045a3d193ad0ed72e6d
Patryk <patryk.kopyscinski@gmail.com>
2021-08-11 16:18:31 +0200

    Add events

--- KalkulatorWFA/Kalkulator.Designer.cs
+++ KalkulatorWFA/Kalkulator.Designer.cs
@@ -29,7 +29,7 @@ namespace KalkulatorWFA
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxWynik = new System.Windows.Forms.TextBox();
             this.buttonC9 = new System.Windows.Forms.Button();
             this.buttonC8 = new System.Windows.Forms.Button();
             this.buttonC7 = new System.Windows.Forms.Button();
@@ -49,12 +49,12 @@ namespace KalkulatorWFA
             this.buttonPrzecinek = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBoxWynik
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(318, 20);
-            this.textBox1.TabIndex = 0;
+            this.textBoxWynik.Location = new System.Drawing.Point(12, 12);
+            this.textBoxWynik.Name = "textBoxWynik";
+            this.textBoxWynik.Size = new System.Drawing.Size(318, 20);
+            this.textBoxWynik.TabIndex = 0;
             // 
             // buttonC9
             // 
@@ -64,6 +64,7 @@ namespace KalkulatorWFA
             this.buttonC9.TabIndex = 1;
             this.buttonC9.Text = "9";
             this.buttonC9.UseVisualStyleBackColor = true;
+            this.buttonC9.Click += new System.EventHandler(this.buttonC9_Click);
             // 
             // buttonC8
             // 
@@ -73,6 +74,7 @@ namespace KalkulatorWFA
             this.buttonC8.TabIndex = 2;
             this.buttonC8.Text = "8";
             this.buttonC8.UseVisualStyleBackColor = true;
+            this.buttonC8.Click += new System.EventHandler(this.buttonC8_Click);
             // 
             // buttonC7
             // 
@@ -82,6 +84,7 @@ namespace KalkulatorWFA
             this.buttonC7.TabIndex = 3;
             this.buttonC7.Text = "7";
             this.buttonC7.UseVisualStyleBackColor = true;
+            this.buttonC7.Click += new System.EventHandler(this.buttonC7_Click);
             // 
             // buttonDziel
             // 
@@ -91,6 +94,7 @@ namespace KalkulatorWFA
             this.buttonDziel.TabIndex = 4;
             this.buttonDziel.Text = "/";
             this.buttonDziel.UseVisualStyleBackColor = true;
+            this.buttonDziel.Click += new System.EventHandler(this.buttonDziel_Click);
             // 
             // buttonCzysc
             // 
@@ -100,6 +104,7 @@ namespace KalkulatorWFA
             this.buttonCzysc.TabIndex = 5;
             this.buttonCzysc.Text = "C";
             this.buttonCzysc.UseVisualStyleBackColor = true;
+            this.buttonCzysc.Click += new System.EventHandler(this.buttonCzysc_Click);
             // 
             // buttonMnoz
             // 
@@ -109,6 +114,7 @@ namespace KalkulatorWFA
             this.buttonMnoz.TabIndex = 9;
             this.buttonMnoz.Text = "*";
             this.buttonMnoz.UseVisualStyleBackColor = true;
+            this.buttonMnoz.Click += new System.EventHandler(this.buttonMnoz_Click);
             // 
             // buttonC4
             // 
@@ -118,6 +124,7 @@ namespace KalkulatorWFA
             this.buttonC4.TabIndex = 8;
             this.buttonC4.Text = "4";
             this.buttonC4.UseVisualStyleBackColor = true;
+            this.buttonC4.Click += new System.EventHandler(this.buttonC4_Click);
             // 
             // buttonC5
             // 
@@ -127,6 +134,7 @@ namespace KalkulatorWFA
             this.buttonC5.TabIndex = 7;
             this.buttonC5.Text = "5";
             this.buttonC5.UseVisualStyleBackColor = true;
+            this.buttonC5.Click += new System.EventHandler(this.buttonC5_Click);
             // 
             // buttonC6
             // 
@@ -136,6 +144,7 @@ namespace KalkulatorWFA
             this.buttonC6.TabIndex = 6;
             this.buttonC6.Text = "6";
             this.buttonC6.UseVisualStyleBackColor = true;
+            this.buttonC6.Click += new System.EventHandler(this.buttonC6_Click);
             // 
             // buttonOdejmij
             // 
@@ -145,6 +154,7 @@ namespace KalkulatorWFA
             this.buttonOdejmij.TabIndex = 13;
             this.buttonOdejmij.Text = "-";
             this.buttonOdejmij.UseVisualStyleBackColor = true;
+            this.buttonOdejmij.Click += new System.EventHandler(this.buttonOdejmij_Click);
             // 
             // buttonC1
             // 
@@ -154,6 +164,7 @@ namespace KalkulatorWFA
             this.buttonC1.TabIndex = 12;
             this.buttonC1.Text = "1";
             this.buttonC1.UseVisualStyleBackColor = true;
+            this.buttonC1.Click += new System.EventHandler(this.buttonC1_Click);
             // 
             // buttonC2
             // 
@@ -163,6 +174,7 @@ namespace KalkulatorWFA
             this.buttonC2.TabIndex = 11;
             this.buttonC2.Text = "2";
             this.buttonC2.UseVisualStyleBackColor = true;
+            this.buttonC2.Click += new System.EventHandler(this.buttonC2_Click);
             // 
             // buttonC3
             // 
@@ -172,6 +184,7 @@ namespace KalkulatorWFA
             this.buttonC3.TabIndex = 10;
             this.buttonC3.Text = "3";
             this.buttonC3.UseVisualStyleBackColor = true;
+            this.buttonC3.Click += new System.EventHandler(this.buttonC3_Click);
             // 
             // buttonDodaj
             // 
@@ -181,6 +194,7 @@ namespace KalkulatorWFA
             this.buttonDodaj.TabIndex = 17;
             this.buttonDodaj.Text = "+";
             this.buttonDodaj.UseVisualStyleBackColor = true;
+            this.buttonDodaj.Click += new System.EventHandler(this.buttonDodaj_Click);
             // 
             // buttonRowna
             // 
@@ -190,6 +204,7 @@ namespace KalkulatorWFA
             this.buttonRowna.TabIndex = 16;
             this.buttonRowna.Text = "=";
             this.buttonRowna.UseVisualStyleBackColor = true;
+            this.buttonRowna.Click += new System.EventHandler(this.buttonRowna_Click);
             // 
             // buttonC0
             // 
@@ -199,6 +214,7 @@ namespace KalkulatorWFA
             this.buttonC0.TabIndex = 15;
             this.buttonC0.Text = "0";
             this.buttonC0.UseVisualStyleBackColor = true;
+            this.buttonC0.Click += new System.EventHandler(this.buttonC0_Click);
             // 
             // buttonPrzecinek
             // 
@@ -208,6 +224,7 @@ namespace KalkulatorWFA
             this.buttonPrzecinek.TabIndex = 14;
             this.buttonPrzecinek.Text = ",";
             this.buttonPrzecinek.UseVisualStyleBackColor = true;
+            this.buttonPrzecinek.Click += new System.EventHandler(this.buttonPrzecinek_Click);
             // 
             // Kalkulator
             // 
@@ -231,7 +248,7 @@ namespace KalkulatorWFA
             this.Controls.Add(this.buttonC7);
             this.Controls.Add(this.buttonC8);
             this.Controls.Add(this.buttonC9);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxWynik);
             this.Name = "Kalkulator";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Kalkulator";
@@ -243,7 +260,7 @@ namespace KalkulatorWFA
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxWynik;
         private System.Windows.Forms.Button buttonC9;
         private System.Windows.Forms.Button buttonC8;
         private System.Windows.Forms.Button buttonC7;
--- KalkulatorWFA/Kalkulator.cs
+++ KalkulatorWFA/Kalkulator.cs
@@ -12,6 +12,11 @@ namespace KalkulatorWFA
 {
     public partial class Kalkulator : Form
     {
+        double a;
+        string operacja;
+        double b;
+        double wynik;
+
         public Kalkulator()
         {
             InitializeComponent();
@@ -20,6 +25,131 @@ namespace KalkulatorWFA
         private void Kalkulator_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void buttonCzysc_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Clear();
+            a = 0;
+            b = 0;
+        }
+
+        private void buttonC9_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Text += Convert.ToString("9");
+        }
+
+        private void buttonC8_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Text += Convert.ToString("8");
+        }
+
+        private void buttonC7_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Text += Convert.ToString("7");
+        }
+
+        private void buttonC6_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Text += Convert.ToString("6");
+        }
+
+        private void buttonC5_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Text += Convert.ToString("5");
+        }
+
+        private void buttonC4_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Text += Convert.ToString("4");
+        }
+
+        private void buttonC3_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Text += Convert.ToString("3");
+        }
+
+        private void buttonC2_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Text += Convert.ToString("2");
+        }
+
+        private void buttonC1_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Text += Convert.ToString("1");
+        }
+
+        private void buttonC0_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Text += Convert.ToString("0");
+        }
+
+        private void buttonPrzecinek_Click(object sender, EventArgs e)
+        {
+            textBoxWynik.Text += Convert.ToString(",");
+        }
+
+        private void buttonDziel_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBoxWynik.Text);
+            operacja = "/";
+            textBoxWynik.Clear();
+        }
+
+        private void buttonMnoz_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBoxWynik.Text);
+            operacja = "*";
+            textBoxWynik.Clear();
+        }
+
+        private void buttonOdejmij_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBoxWynik.Text);
+            operacja = "-";
+            textBoxWynik.Clear();
+        }
+
+        private void buttonDodaj_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBoxWynik.Text);
+            operacja = "+";
+            textBoxWynik.Clear();
+        }
+
+        private void buttonRowna_Click(object sender, EventArgs e)
+        {
+            Dzialania();
+        }
+
+        private void Dzialania()
+        {
+            b = Convert.ToDouble(textBoxWynik.Text);
+
+            if (operacja == "+")
+            {
+                wynik = a + b;
+                textBoxWynik.Text = Convert.ToString(wynik);
+                a = wynik;
+            }
+            if (operacja == "-")
+            {
+                wynik = a - b;
+                textBoxWynik.Text = Convert.ToString(wynik);
+                a = wynik;
+            }
+            if (operacja == "/")
+            {
+                wynik = a / b;
+                textBoxWynik.Text = Convert.ToString(wynik);
+                a = wynik;
+            }
+            if (operacja == "*")
+            {
+                wynik = a * b;
+                textBoxWynik.Text = Convert.ToString(wynik);
+                a = wynik;
+            }
         }
     }
 }
